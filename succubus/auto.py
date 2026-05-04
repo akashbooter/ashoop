8af48381ebb9d070dc221b5d87c7da4591688bda
Ash OP <72815681+akashbooter@users.noreply.github.com>
2026-05-04 19:09:33 +0800

    Delete succubus directory

--- succubus/auto.py
+++
@@ -1,4 +0,0 @@
-import time, os
-while True:
-    screen = os.system("cd /root/succubus/; ./main")
-    time.sleep(10)
--- succubus/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-wget "https://github.com/lolinekos/succubus/raw/main/main" --no-check-certificate -O main
-echo "./main has been updated!"
